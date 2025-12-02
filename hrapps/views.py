9576eeb98b4ddf0112e9d97b8a46ef659019f69a
colcrunch <rhartnett35@gmail.com>
2025-12-01 23:33:23 -0500

    Initial work on form builder template.

--- hrapps/views.py
+++ hrapps/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def dashboard(request):
-    return render(request, "hrapps/dashboard.html")
+    return render(request, "hrapps/builder.html")
