54c80fef429c8b4c62b192241392a5b70669d464
colcrunch <rhartnett35@gmail.com>
2025-12-26 15:36:58 -0500

    Register form model on admin pages.

--- hrapps/admin.py
+++ hrapps/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Form
+
 
 # Register your models here.
+class FormAdmin(admin.ModelAdmin):
+    list_select_related = True
+    list_display = ('name', 'active', 'corporation','updated', 'created')
+    list_filter = ('corporation__corporation_name','active')
+
+    search_fields = ('name','corporation__corporation_name')
+
+admin.site.register(Form, FormAdmin)
