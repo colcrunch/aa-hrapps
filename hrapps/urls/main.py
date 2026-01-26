d188776fb6a3a21abf0675b9e99ee20f201646c9
colcrunch <rhartnett35@gmail.com>
2026-01-26 01:08:16 -0500

    Start work on application flow.

--- hrapps/urls/main.py
+++ hrapps/urls/main.py
@@ -6,4 +6,5 @@ app_name = 'hrapps'
 
 urlpatterns = [
     re_path(r"^$", views.dashboard, name="dashboard"),
+    re_path(r"^apply/(?P<form_id>\d+)/$", views.apply, name="apply"),
 ]
--- hrapps/views/main.py
+++ hrapps/views/main.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
 
+from . import Field
+from hrapps.models import FormResponse, Form
+
+
 def dashboard(request):
-    return render(request, "hrapps/main/dashboard.html")
+    user = request.user
+    applications = FormResponse.objects.filter(user=user)
+
+    recruiting_corps = Form.objects.filter(active=True)
+
+    ctx = {
+        "applications": applications,
+        "corp_forms": recruiting_corps
+    }
+    return render(request, "hrapps/main/dashboard.html", ctx)
+
+
+def apply(request, form_id):
+    form = Form.objects.get(pk=form_id)
+
+    fields = []
+    for field in form.fields:
+        fields.append(Field(**field))
+
+    return render(request, "hrapps/main/apply.html", {"form": form, "fields": fields})
