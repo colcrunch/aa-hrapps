ccb21cf38fc3455891bd6b6e4e14bfb80cdaf9a4
colcrunch <rhartnett35@gmail.com>
2026-07-12 08:09:53 +0000

    Add template tag to filter attachment queryset.

--- hrapps/templatetags/hrtags.py
+++ hrapps/templatetags/hrtags.py
@@ -3,3 +3,9 @@ from django.template.defaulttags import register
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def filter_by_question_id(qs, question_id):
+    return qs.filter(
+        question_id=question_id
+    )
