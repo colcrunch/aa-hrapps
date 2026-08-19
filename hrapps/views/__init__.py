352b359321a7b7ec698e8c2172cc64ac3dd23af9
colcrunch <rhartnett35@gmail.com>
2026-08-19 19:57:37 +0000

    Add withdrawn to final states

--- hrapps/views/__init__.py
+++ hrapps/views/__init__.py
@@ -169,7 +169,7 @@ def get_application_context(request, application_id, admin=False):
       "memberaudit": memberaudit,
       "admin": admin,
       "attachments": attachments,
-      "final_states": ["approved", "rejected"]
+      "final_states": ["approved", "rejected", "withdrawn"]
     }
 
 def add_comment(request, response_id):
